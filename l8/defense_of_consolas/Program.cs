b48ff8285c0da24eb393e46790f0b459c9b9cf03
Michael Cupka <michaelcupka@hotmail.com>
2022-06-01 18:03:38 -0400

    l8: added l8 solutions

---
+++ l8/defense_of_consolas/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Target Row? ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Target Column? ");
+int column = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Deploy to:");
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+Console.BackgroundColor = ConsoleColor.Gray;
+Console.WriteLine($"({row}, {column - 1})");
+Console.WriteLine($"({row - 1}, {column})");
+Console.WriteLine($"({row}, {column + 1})");
+Console.WriteLine($"({row + 1}, {column})");
+Console.Title = "Defense of Consolas";
+Console.Beep();
